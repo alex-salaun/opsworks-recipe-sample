9fd61e99c7b2d3e4c3a573595229251ad748b44a
Alexandre Salaun <asalaun@siliconsalad.com>
2015-02-27 09:58:38 +0100

    Initial commit

---
+++ nginx-custom/recipes/configure.rb
@@ -0,0 +1,28 @@
+Chef::Log.info("Configure Nginx")
+
+node[:deploy].each do |app_name, deploy|
+  Chef::Log.info("Debug : #{deploy[:application_type]}")
+  Chef::Log.info("Debug : #{deploy}")
+
+  if defined?(deploy[:application_type]) && deploy[:application_type] == 'static'
+    Chef::Log.info("Update conf file : /etc/nginx/sites-available/#{app_name.gsub('-', '_')}")
+
+    template "/etc/nginx/sites-available/#{app_name.gsub('-', '_')}" do
+      source "site.erb"
+
+      variables(
+        :proxy_url  => deploy[:environment][:proxy_url],
+        :app_name   => app_name.gsub('-', '_'),
+        :domain     => (deploy[:domains].first)
+      )
+    end
+
+    service "nginx" do
+      action :reload
+    end
+
+    service "nginx" do
+      action :restart
+    end
+  end
+end
